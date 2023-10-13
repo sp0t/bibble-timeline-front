99bc748fe64a9a6247d93a1d4271c9990c3d7985
sp0t <chenyin1g992@gmail.com>
2023-10-13 18:18:33 -0300

    fix: all bugs

--- frontend/timeline-backend/admin/config/api.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rest: {
-    defaultLimit: 1000,
-    maxLimit: 1000,
-    withCount: false,
-  },
-};
--- frontend/timeline-backend/admin/config/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  // port: env.int('PORT', 80),
-  // url: env("PUBLIC_URL", "http://timelineapi.testing.929.org.il:80"),
-  port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "http://127.0.0.1:1337"),
-});
